8eb344baf86011718f99d37f0037b16d38249a76
Luan Lima <lordellu@hotmail.com>
2023-08-10 22:10:43 -0300

    criando endpoints da api sem conexao ao banco de dados

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,37 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")] // Define a rota do endpoint como o nome da classe sem o "controller"
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
+
+    [HttpPost] //Especifica o verbo HTTP
+    public IActionResult AdicionaFilme([FromBody] Filme filme) //anotacao fromBody para parametro
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme); // retorna 201 com o objeto criado e o location no header
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    // Interrogacao especifica que pode retornar valor nulo ou do tipo Filme
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound(); // retorna o codigo 404
+        return Ok(filme); // retorna 200 e o objeto
+    }
+}
+
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O titulo do filme é obrigatório!")]
+    [StringLength(30, ErrorMessage = "Limite máximo de caracteres é 30!")]
+    public String Titulo { get; set; }
+
+    [Required(ErrorMessage = "O genero do filme é obrigatório!")]
+    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres!")]
+    public String Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatório!")]
+    [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos!")]
+    public int Duracao { get; set; }
+
+}
+
