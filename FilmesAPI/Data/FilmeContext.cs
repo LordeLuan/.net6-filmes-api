8227ea992c2f153a9704939da9c20c05a14d9208
Luan Lima <lordellu@hotmail.com>
2023-08-10 22:30:19 -0300

    implementado conexao com banco de dados mysql com entity framework

---
+++ FilmesAPI/Data/FilmeContext.cs
@@ -0,0 +1,19 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+
+        public FilmeContext(DbContextOptions<FilmeContext> opts) : base(opts) // Casa o parametro para o construtor da classe DbContext
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+
+
+
+    }
+}
